7229a30feadf667d25c1618ccbd44f5b23d52702
Lifra.devs <bundukineltonjohn@gmail.com>
2025-12-22 22:48:20 +0800

    z

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,13 +33,19 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
+
+    // Google Sign In
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+
+    // Facebook Login
+    implementation("com.facebook.android:facebook-login:16.2.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
